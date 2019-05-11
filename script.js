370cf43b358272baa36bb49ed88005b942400eec
Darren Rambaud <xyzst@users.noreply.github.com>
2019-05-10 21:30:10 -0500

    Added coding challenge statement

--- script.js
+++ script.js
@@ -541,3 +541,26 @@
 // darrenES6.calcAge();
 // darrenES6.wonMedal(); // 1
 // darrenES6.wonMedal(); // 2
+
+/**
+ * Coding Challenge
+ * Suppose that you're working in a small town administration and you're in charge of two town elements
+ *  1. Parks
+ *  2. Streets
+ *
+ * It's a very small town so right now there are only 3 parks and 4 streets. All parks and streets have
+ * a name and a build year
+ *
+ * At an end of year meeting, your boss wants a final report with the following:
+ *  1. Tree density of each park in the town (formula: # of trees / park area)
+ *  2. Average age of each  town's park (formula: sum of all ages / number of parks)
+ *  3. The name of the park that has more than 1000 trees
+ *  4. Total and average length of the town's streets
+ *  5. Size classification of all streets:
+ *      tiny/small/normal/big/huge
+ *      If the size is unknown, default is 'normal'
+ *
+ * All the report data should be printed to the console
+ * HINT: use ES6 features: classes, subclasses, template strings, default parameters, maps, arrow functions,
+ * destructuring ...
+ */
